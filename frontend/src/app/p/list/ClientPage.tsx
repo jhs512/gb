e8ef2bd819a6c4bddb2fd46f4175c2bab9d13cf3
jhs512 <jangka512@gmail.com>
2024-10-29 01:53:59 +0900

    ui, paging

--- frontend/src/app/p/list/ClientPage.tsx
+++ frontend/src/app/p/list/ClientPage.tsx
@@ -1,28 +1,52 @@
 "use client";
 
+import { Page } from "@/app/types/Page";
 import { Post } from "@/app/types/Post";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 export default function ClientPage() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [postPage, setPostPage] = useState<Page<Post> | null>(null);
+
+  // 여기서 쿼리 파라미터 받아오기
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page") ?? "1";
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/v1/posts")
+    fetch(`http://localhost:8080/api/v1/posts?page=${page}`)
       .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
+      .then((data) => setPostPage(data));
+  }, [page]);
 
   return (
     <>
       <div className="grid">
-        {posts.map((post) => (
-          <Link href={`/p/${post.id}`} key={post.id}>
-            {post.title}
-          </Link>
-        ))}
+        {postPage && (
+          <>
+            {postPage.content.map((post) => (
+              <Link href={`/p/${post.id}`} key={post.id}>
+                {post.author.nickname}
+                {post.title}
+              </Link>
+            ))}
+          </>
+        )}
       </div>
 
       <Link href="/p/write">글쓰기</Link>
+
+      <div>
+        {postPage && (
+          <div className="flex gap-2">
+            {/* postPage.totalPages 로 반복문 */}
+            {Array.from({ length: postPage.totalPages }).map((_, index) => (
+              <Link href={`/p/list?page=${index + 1}`} key={index}>
+                {index + 1}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
