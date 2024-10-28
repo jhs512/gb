0172e20d821e33d8fc1b807fae005e0d4d064586
jhs512 <jangka512@gmail.com>
2024-10-29 02:15:46 +0900

    page menu arm size 5

--- frontend/src/app/p/list/ClientPage.tsx
+++ frontend/src/app/p/list/ClientPage.tsx
@@ -10,13 +10,16 @@ export default function ClientPage() {
 
   // 여기서 쿼리 파라미터 받아오기
   const searchParams = useSearchParams();
-  const page = searchParams.get("page") ?? "1";
+  const currentPageNumber = parseInt(searchParams.get("page") ?? "1");
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/v1/posts?page=${page}`)
+    fetch(`http://localhost:8080/api/v1/posts?page=${currentPageNumber}`)
       .then((res) => res.json())
       .then((data) => setPostPage(data));
-  }, [page]);
+  }, [currentPageNumber]);
+
+  // 현재 페이지 기준으로 좌우 몇 페이지까지 보여줄지
+  const pageMenuArmSize = 5;
 
   return (
     <>
@@ -35,17 +38,45 @@ export default function ClientPage() {
 
       <Link href="/p/write">글쓰기</Link>
 
-      <div>
-        {postPage && (
-          <div className="flex gap-2">
-            {/* postPage.totalPages 로 반복문 */}
-            {Array.from({ length: postPage.totalPages }).map((_, index) => (
-              <Link href={`/p/list?page=${index + 1}`} key={index}>
-                {index + 1}
+      <div className="flex gap-2">
+        {postPage && currentPageNumber - pageMenuArmSize > 1 && (
+          <>
+            <Link href={`/p/list?page=1`}>1</Link>
+            <div>...</div>
+          </>
+        )}
+        {postPage &&
+          Array.from({ length: 100 }, (_, i) => i + 1)
+            .filter((pageNumber) => {
+              const start = currentPageNumber - pageMenuArmSize;
+
+              const end = Math.min(
+                currentPageNumber + pageMenuArmSize,
+                postPage.totalPages
+              );
+
+              return pageNumber >= start && pageNumber <= end;
+            })
+            .map((pageNumber) => (
+              <Link
+                href={`/p/list?page=${pageNumber}`}
+                key={pageNumber}
+                className={
+                  currentPageNumber === pageNumber ? "text-red-500" : ""
+                }
+              >
+                {pageNumber}
               </Link>
             ))}
-          </div>
-        )}
+        {postPage &&
+          currentPageNumber + pageMenuArmSize < postPage.totalPages && (
+            <>
+              <div>...</div>
+              <Link href={`/p/list?page=${postPage.totalPages}`}>
+                {postPage.totalPages}
+              </Link>
+            </>
+          )}
       </div>
     </>
   );
